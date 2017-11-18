7b2b99f13b3f4e25d4181ec69d59ecebc369d8b5
Raju <meetrajnichit@gmail.com>
2017-11-18 10:53:43 +0530

    Initial commit

---
+++ src/main/java/com/example/rest/springbootcommonutility/SpringBootCommonUtilityApplication.java
@@ -0,0 +1,12 @@
+package com.example.rest.springbootcommonutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommonUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommonUtilityApplication.class, args);
+	}
+}
